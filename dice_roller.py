d1ab0400b4aa4e67ac45ac75939ab7d95218dc38
TomThemeles <146465681+TomThemeles@users.noreply.github.com>
2025-05-04 20:03:52 -0400

    dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -9,5 +9,4 @@ st.subheader("Roll a die with values: 0, 2, 4, 6, 8, 10")
 # Button to trigger roll
 if st.button("Roll the Dice"):
     result = random.choice([0, 2, 4, 6, 8, 10])
-    st.balloons()
     st.success(f"You rolled a **{result}**!")
